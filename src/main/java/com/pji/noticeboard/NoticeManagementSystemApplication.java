3ad3c859299210b30b31eb270153fe79fb8d73d1
편준일 <joon2344@qoo10.com>
2024-07-19 19:18:11 +0900

    Initial commit

---
+++ src/main/java/com/pji/noticeboard/NoticeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pji.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pji/noticeboard/NoticeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pji.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
