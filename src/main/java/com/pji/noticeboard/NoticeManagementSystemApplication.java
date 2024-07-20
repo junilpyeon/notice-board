15cce3a7dbbb67a6b915053d48b2b628f787b513
편준일 <joon2344@qoo10.com>
2024-07-20 23:31:42 +0900

    feat: README 파일 환경별 구분 (30m)

--- src/main/java/com/pji/noticeboard/NoticeManagementSystemApplication.java
+++ src/main/java/com/pji/noticeboard/NoticeManagementSystemApplication.java
@@ -3,9 +3,11 @@ package com.pji.noticeboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class NoticeManagementSystemApplication {
 
     public static void main(String[] args) {
